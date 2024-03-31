e7397e6b33be138f852d1cc978917a0377f57b15
CiarDaire <SakokuEdicts26>
2024-04-01 11:05:16 +1300

    view booking works, need to update with admin

--- viewbooking.php
+++ viewbooking.php
@@ -17,6 +17,35 @@ Details will be updated after any edits/updates have been made. -->
     </style>
 </head>
 <body>
+    <?php
+
+    // assigns callable variable to database connection and provides error message if connection is unavailable
+    include "config.php";
+    $DBC = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBDATABASE);
+    if (mysqli_connect_errno()) {
+        echo "Error: Unable to connect to MYSQL.". mysqli_connect_error();
+        exit();
+    };
+
+    // if id exists 
+    if ($_SERVER["REQUEST_METHOD"] == "GET"){
+        $id = $_GET['id'];
+        // if its empty or not a numerical data
+        if(empty($id) or !is_numeric($id)){
+            echo '<h2>The booking ID is invalid.</h2>';
+            exit();
+        }
+    }
+
+    $query = 'SELECT booking.bookingID, room.roomname, booking.checkinDate, booking.checkoutDate, booking.contactNumber, booking.extras, booking.roomReview
+    FROM booking
+    INNER JOIN room ON booking.roomID = room.roomID
+    WHERE booking.bookingID = ' .$id;
+
+    $result = mysqli_query($DBC, $query);
+    $rowcount = mysqli_num_rows($result);
+
+    ?>
     <div class="booking-form">
         <h3>Logged in as Test</h3>
         <h1>Booking Details View</h1>
@@ -24,24 +53,24 @@ Details will be updated after any edits/updates have been made. -->
             <!-- Would suggest a replacement to "Bookings" to keep it consistent with the other pages -->
             <h2><a href="listbookings.php">[Return to the booking listing]</a><a href="/bnb/">[Return to the main page]</a></h2>
         </div>
-        <fieldset>
-            <!-- Assumption: room detail number will be replaced with the bookingID value -->
-            <!-- Suggest this is changed to Booking Detail Number (#bookingID) instead of Room Detail Number -->
-            <legend>Room detail #2</legend>
-            <!-- Details will be replaced with respective booking data -->
-            <p class="field">Room name:</p>
-            <p class="detail">Kellie</p>
-            <p class="field">Checkin date:</p>
-            <p class="detail">2018-09-15</p>
-            <p class="field">Checkout date:</p>
-            <p class="detail">2018-09-19</p>
-            <p class="field">Contact number:</p>
-            <p class="detail">(001) 123 1234</p>
-            <p class="field">Extras:</p>
-            <p class="detail">nothing</p>
-            <p class="field">Room review:</p>
-            <p class="detail">nothing</p>
-        </fieldset>
+        <!-- Have changed "room" detail to "booking" detail to fix what is being viewed, and will use booking id number instead of room id -->
+        <?php
+        if ($rowcount > 0){
+            echo "<fieldset><legend>Booking detail #$id</legend><dl>";
+            $row = mysqli_fetch_assoc($result);
+            echo "<dt>Room name:</dt><dd>" .$row["roomname"] ."</dd>" .PHP_EOL;
+            echo "<dt>Checkin date:</dt><dd>" .$row["checkinDate"] ."</dd>" .PHP_EOL;
+            echo "<dt>Checkout date:</dt><dd>" .$row["checkoutDate"] ."</dd>" .PHP_EOL;
+            echo "<dt>Contact number:</dt><dd>" .$row["contactNumber"] ."</dd>" .PHP_EOL;
+            echo "<dt>Extras:</dt><dd>" .$row["extras"] ."</dd>" .PHP_EOL;
+            echo "<dt>Room review</dt><dd>" .$row["roomReview"] ."</dd>" .PHP_EOL;
+            echo "</dl></fieldset>" .PHP_EOL;
+        }else{
+            echo "<h2>No booking exists.</h2>";
+            mysqli_free_result($result);
+            mysqli_close($DBC);
+        }
+        ?>
     </div>
 </body>
 </html>
