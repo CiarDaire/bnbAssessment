3dffc7d6bcd2b88ab1a23848e3c1227aa55d0bc7
CiarDaire <SakokuEdicts26>
2024-04-19 17:37:08 +1200

    update db

--- BandB.sql
+++ BandB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 06, 2024 at 12:37 PM
+-- Generation Time: Apr 19, 2024 at 06:11 AM
 -- Server version: 8.0.23
 -- PHP Version: 8.0.3
 
@@ -43,12 +43,22 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`bookingID`, `checkinDate`, `checkoutDate`, `contactNumber`, `extras`, `roomReview`, `roomID`, `customerID`) VALUES
-(3, '2024-03-05', '2024-03-11', '(009) 546 7464', 'nothing', 'something', 8, 13),
+(3, '2024-03-05', '2024-03-11', '(009) 546 7464', 'nothing NEW NEW', 'something NEW', 5, 13),
 (11, '2018-09-16', '2018-09-23', '(093) 434 3434', 'nothing', 'new review', 9, 15),
 (13, '2018-09-15', '2018-09-29', '(093) 434 3434', 'nofhdf', NULL, 11, 18),
 (18, '2018-09-16', '2018-09-22', '(093) 434 3434', 'mk,mf', NULL, 6, 10),
 (19, '2018-09-30', '2018-10-28', '(093) 434 3434', 'nfdmnfdmf', NULL, 12, 16),
-(21, '2018-10-07', '2018-10-11', '(093) 434 3434', 'nmn', NULL, 4, 12);
+(21, '2018-10-07', '2018-10-11', '(093) 434 3434', 'nmn', NULL, 4, 12),
+(22, '2018-09-09', '2018-09-11', '(093) 434 3434', 'nothing', NULL, 7, 13),
+(26, '2018-10-08', '2018-10-13', '(093) 434 3434', 'nothing', NULL, 1, 1),
+(27, '2018-09-04', '2018-09-19', '(444) 345 5643', 'nothing', NULL, 12, 9),
+(30, '2018-09-26', '2018-10-01', '(555) 556 5566', 'fdfdf', NULL, 8, 8),
+(32, '2018-09-16', '2018-09-23', '(666) 777 8888', 'nothing', NULL, 13, 18),
+(34, '2018-10-22', '2018-10-24', '(444) 345 5643', 'vcv', NULL, 6, 16),
+(43, '2018-10-01', '2018-10-03', '(666) 456 4567', 'bjhb', NULL, 3, 10),
+(46, '2024-03-13', '2018-10-24', '(093) 434 3433', 'vcv', NULL, 1, 1),
+(51, '2018-09-10', '2018-09-17', '(093) 434 3434', 'nothing', NULL, 13, 21),
+(53, '2018-09-02', '2018-09-09', '(555) 556 5566', 'NEW EXTRA', ' ', 4, 19);
 
 -- --------------------------------------------------------
 
@@ -73,10 +83,9 @@ INSERT INTO `customer` (`customerID`, `firstname`, `lastname`, `email`, `passwor
 (2, 'Desiree', 'Collier', 'Maecenas@non.co.uk', ''),
 (3, 'Irene', 'Walker', 'id.erat.Etiam@id.org', ''),
 (4, 'Forrest', 'Baldwin', 'eget.nisi.dictum@a.com', ''),
-(5, 'Beverly', 'Sellers', 'ultricies.sem@pharetraQuisqueac.co.uk', ''),
 (6, 'Glenna', 'Kinney', 'dolor@orcilobortisaugue.org', ''),
 (7, 'Montana', 'Gallagher', 'sapien.cursus@ultriciesdignissimlacus.edu', ''),
-(8, 'Harlan', 'Lara', 'Duis@aliquetodioEtiam.edu', ''),
+(8, 'Harlan', 'Lara', 'Duis@aliquetodioEtiam.com', ''),
 (9, 'Benjamin', 'King', 'mollis@Nullainterdum.org', ''),
 (10, 'Rajah', 'Olsen', 'Vestibulum.ut.eros@nequevenenatislacus.ca', ''),
 (11, 'Castor', 'Kelly', 'Fusce.feugiat.Lorem@porta.co.uk', ''),
@@ -84,7 +93,7 @@ INSERT INTO `customer` (`customerID`, `firstname`, `lastname`, `email`, `passwor
 (13, 'Porter', 'Leonard', 'dui.Fusce@accumsanlaoreet.net', ''),
 (14, 'Buckminster', 'Gaines', 'convallis.convallis.dolor@ligula.co.uk', ''),
 (15, 'Hunter', 'Rodriquez', 'ridiculus.mus.Donec@est.co.uk', ''),
-(16, 'Zahir', 'Harper', 'vel@estNunc.com', ''),
+(16, 'Zahir', 'Harper', 'velest@Nunc.com', ''),
 (17, 'Sopoline', 'Warner', 'vestibulum.nec.euismod@sitamet.co.uk', ''),
 (18, 'Burton', 'Parrish', 'consequat.nec.mollis@nequenonquam.org', ''),
 (19, 'Abbot', 'Rose', 'non@et.ca', ''),
@@ -118,7 +127,7 @@ INSERT INTO `room` (`roomID`, `roomname`, `description`, `roomtype`, `beds`) VAL
 (7, 'Helen', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus.', 'S', 2),
 (8, 'Octavia', 'Lorem ipsum dolor sit amet,', 'D', 3),
 (9, 'Gretchen', 'Lorem ipsum dolor sit', 'D', 3),
-(10, 'Bernard', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer', 'S', 5),
+(10, 'Bernard', 'King beds', 'D', 1),
 (11, 'Dacey', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur', 'D', 2),
 (12, 'Preston', 'Lorem', 'D', 2),
 (13, 'Dane', 'Lorem ipsum dolor', 'S', 4),
@@ -156,7 +165,7 @@ ALTER TABLE `room`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `bookingID` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `bookingID` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `customer`
@@ -168,7 +177,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `roomID` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `roomID` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
